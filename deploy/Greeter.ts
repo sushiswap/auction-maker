b45afe4f341c070e024ea9a9ea12a6477512c484
Sarang Parikh <sarangparikh22@gmail.com>
2022-03-07 23:50:13 +0530

    test init

--- deploy/Greeter.ts
+++ deploy/Greeter.ts
@@ -5,16 +5,7 @@ const func: DeployFunction = async function ({
   getNamedAccounts,
   deployments,
 }: HardhatRuntimeEnvironment) {
-  const { deploy } = deployments;
-
-  const { deployer } = await getNamedAccounts();
-
-  const { address } = await deploy("Greeter", {
-    from: deployer,
-    args: ["Hello, world!"],
-  });
-
-  console.log(`Greeter deployed to ${address}`);
+  
 };
 
 export default func;
