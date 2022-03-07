882180091ae7bae8db648e9ba85bab42dd291b8d
Sarang Parikh <sarangparikh22@gmail.com>
2022-03-08 00:02:07 +0530

    removed libs

--- contracts/SushiMakerAuction.sol
+++ contracts/SushiMakerAuction.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.11;
 
-import "./libraries/UniswapV2Library.sol";
+import "@sushiswap/core/contracts/uniswapv2/interfaces/IUniswapV2Pair.sol";
 import "./utils/BoringBatchable.sol";
 import "./utils/BoringOwnable.sol";
 
